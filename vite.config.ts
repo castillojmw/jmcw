6afd83c4bd5bbd927ab96336a4057f72e87a31b9
castillojmw <castillojmw.ph@gmail.com>
2026-04-04 22:54:25 +0100

    vite config updates

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/jmcw/' : '/',
+  base: '/jmcw/',
   server: {
     host: "0.0.0.0",
     allowedHosts: ["localhost", "wide-socks-wink.loca.lt"],
